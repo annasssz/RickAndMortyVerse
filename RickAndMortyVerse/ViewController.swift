b690ae3cf348ec7beb8641917148a328b5fd8d19
anamepisashvili <ana.mepisashvili@gmail.com>
2024-02-28 08:48:48 +0400

    initial commit

---
+++ RickAndMortyVerse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyVerse
+//
+//  Created by Ana Mepisashvili on 2/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
